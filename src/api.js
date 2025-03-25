d2c4874c7b7977940057ac6bc716822a6db64b78
Midhun <midhun.t@webandcrafts.in>
2025-03-25 11:55:04 +0530

    Updated

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backend-scheduling-vvbm.onrender.com", 
+  baseURL: "https://backend-scheduling-vvbm.onrender.com/api", 
   headers: { "Content-Type": "application/json" },
 });
 
